688168e58bca7f9547707341d156239ffe0db0eb
Ramielson <ramielsonedgar@gmail.com>
2023-06-15 14:23:29 +0300

    test-10

--- js/cashback.js
+++ js/cashback.js
@@ -2,24 +2,15 @@ const button = document.querySelector('.btn-filter')
 const filterMenu = document.querySelector('.filter-dropdown')
 const filterList = document.querySelector('.statistics-filter__list')
 const filterItem = document.querySelectorAll('.statistics-filter__list-item')
- 
+const progressBar = document.querySelectorAll('.progress-bar')
+
 //fix list padding//
-if(filterItem.length > 5) {
+if (filterItem.length > 5) {
     filterList.style.paddingRight = '15px'
 }
 
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     filterMenu.classList.toggle('show')
 })
 
-//color generator // 
-
-function generateColorProgressBar () {
-    const randomRgb = 'rgba(' +  Math.round(Math.random()*255) + ',' +  Math.round(Math.random()*255) + ',' +  Math.round(Math.random()*255) + ')';
-}
-
-generateColorProgressBar ()
-
-
- 
