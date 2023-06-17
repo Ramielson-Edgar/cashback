a5bcbcf17f929276f9662ae7c32df3f889338e8a
Ramielson <ramielsonedgar@gmail.com>
2023-06-17 20:24:08 +0300

    fix logic

--- js/cashback.js
+++ js/cashback.js
@@ -2,67 +2,65 @@ const button = document.querySelector('.btn-filter')
 const filterItem = document.querySelectorAll('.statistics-filter__list-item')
 const filterList = document.querySelector('.statistics-filter__list')
 const inputGroup = document.querySelector('.filter-group')
+const scrollContainer = document.querySelector('.statistics-filter-wrapper');
+
+//on handle active filter //
+button.addEventListener("click", () => {
+    inputGroup.classList.toggle('is-active')
+
+    if (inputGroup.classList.contains('is-active')) {
+        filterList.style.paddingRight = "15px"
+    }
+
+    if (filterItem.length > 6 && inputGroup.classList.contains('is-active')) {
+        filterList.style.paddingRight = "15px"
+    }
+
+
+    if (filterItem.length === 6 && !inputGroup.classList.contains('is-active')) {
+        filterList.style.paddingRight = "0"
+    }
 
 
 
-if (!inputGroup.classList.contains('isActive') && filterItem.length >  6) {
-    filterList.style.paddingRight = '15px'
+    scrollContainer.classList.toggle('is-active')
+
+})
+
+
+//check if item more than 6 do something.. //
+if (filterItem.length > 6) {
+    filterList.style.paddingRight = "15px"
+} else {
+    filterList.style.paddingRight = "0"
+}
+// if(filterItem.length > 6 && !filterList.classList.contains('is-active')) {
+//     filterList.classList.add('is-active')
+// }  
+
+
+
+//correction on mobile// 
+if (window.innerWidth <= 575) {
+
+    button.addEventListener("click", () => {
+        scrollContainer.classList.toggle('is-active')
+
+        if (filterItem.length >= 6 && !inputGroup.classList.contains('is-active')) {
+            filterList.style.paddingRight = "15px"
+        }
+    })
+
+    if (filterItem.length >= 6) {
+        filterList.style.paddingRight = "15px"
+    } else {
+        filterList.style.paddingRight = "0"
+    }
+
 }
 
-button.addEventListener('click', () => {
-    inputGroup.classList.toggle('isActive')
 
-    //fix list on active//
-    if (inputGroup.classList.contains('isActive') && filterItem.length > 5) {
-        document.querySelector('.statistics-filter-wrapper').style.height = "70%";
-        filterList.style.paddingRight = '15px';
-    } else {
-        document.querySelector('.statistics-filter-wrapper').style.height = "85%";
-    }
 
-    if (filterItem.length <= 6) {
-        filterList.style.paddingRight = '0';
-    }
-
-    if (filterItem.length === 6 && inputGroup.classList.contains('isActive')) {
-        filterList.style.paddingRight = '15px';
-    }
-})
-
-//correction padding list on mobile
-window.addEventListener('resize',(e) =>{
-    if (e.target.innerWidth <= 575) {
-
-    
-        
-        button.addEventListener('click', () => {
-           
-            //fix list on active//
-            if (inputGroup.classList.contains('isActive') && filterItem.length > 6) {
-                document.querySelector('.statistics-filter-wrapper').style.height = "40%";
-            } else {
-                document.querySelector('.statistics-filter-wrapper').style.height = "60%";
-            }
-    
-        })
-    }
-
-})
- 
-// if (window.innerWidth <= 575) {
-
-    
-//     button.addEventListener('click', () => {
-       
-//         //fix list on active//
-//         if (inputGroup.classList.contains('isActive') && filterItem.length > 6) {
-//             document.querySelector('.statistics-filter-wrapper').style.height = "40%";
-//         } else {
-//             document.querySelector('.statistics-filter-wrapper').style.height = "60%";
-//         }
-
-//     })
-// }
 
 //debounce //
 const debounce = (fn, ms) => {
