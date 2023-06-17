39b60376ca78a8e81084a5655acdd269f0d6d549
Ramielson <ramielsonedgar@gmail.com>
2023-06-17 18:38:25 +0300

    test mob item

--- js/cashback.js
+++ js/cashback.js
@@ -3,12 +3,9 @@ const filterItem = document.querySelectorAll('.statistics-filter__list-item')
 const filterList = document.querySelector('.statistics-filter__list')
 const inputGroup = document.querySelector('.filter-group')
 
-const rows = document.querySelectorAll('.report-table__body')
-const tableHeader = document.querySelectorAll('.report-table__header tr th' )
 
 
-//fix list//
-if (filterItem.length > 5 || inputGroup.classList.contains('isActive')) {
+if (!inputGroup.classList.contains('isActive') && filterItem.length >  6) {
     filterList.style.paddingRight = '15px'
 }
 
@@ -16,17 +13,56 @@ button.addEventListener('click', () => {
     inputGroup.classList.toggle('isActive')
 
     //fix list on active//
-    if (inputGroup.classList.contains('isActive')) {
-        document.querySelector('.statistics-filter-wrapper').style.height = "60%";
-        filterList.style.paddingRight = '15px'
+    if (inputGroup.classList.contains('isActive') && filterItem.length > 5) {
+        document.querySelector('.statistics-filter-wrapper').style.height = "70%";
+        filterList.style.paddingRight = '15px';
     } else {
-        document.querySelector('.statistics-filter-wrapper').style.height = "80%"
-        filterList.style.paddingRight = '0'
+        document.querySelector('.statistics-filter-wrapper').style.height = "85%";
     }
 
+    if (filterItem.length <= 6) {
+        filterList.style.paddingRight = '0';
+    }
 
+    if (filterItem.length === 6 && inputGroup.classList.contains('isActive')) {
+        filterList.style.paddingRight = '15px';
+    }
 })
 
+//correction padding list on mobile
+window.addEventListener('resize',(e) =>{
+    if (e.target.innerWidth <= 575) {
+
+    
+        
+        button.addEventListener('click', () => {
+           
+            //fix list on active//
+            if (inputGroup.classList.contains('isActive') && filterItem.length > 6) {
+                document.querySelector('.statistics-filter-wrapper').style.height = "40%";
+            } else {
+                document.querySelector('.statistics-filter-wrapper').style.height = "60%";
+            }
+    
+        })
+    }
+
+})
+ 
+// if (window.innerWidth <= 575) {
+
+    
+//     button.addEventListener('click', () => {
+       
+//         //fix list on active//
+//         if (inputGroup.classList.contains('isActive') && filterItem.length > 6) {
+//             document.querySelector('.statistics-filter-wrapper').style.height = "40%";
+//         } else {
+//             document.querySelector('.statistics-filter-wrapper').style.height = "60%";
+//         }
+
+//     })
+// }
 
 //debounce //
 const debounce = (fn, ms) => {
@@ -47,7 +83,7 @@ function onChange(e) {
         if (!inputGroup.classList.contains('isActive')) {
             e.target.value = ''
             inputGroup.classList.remove('isActive')
-            filterItem.forEach(el => {el.style.display = ""})
+            filterItem.forEach(el => { el.style.display = "" })
         }
     })
     filterItem.forEach(el => {
@@ -60,5 +96,5 @@ onChange = debounce(onChange, 500)
 
 document.querySelector('.filter-control').addEventListener('keyup', onChange)
 
- 
+
 
